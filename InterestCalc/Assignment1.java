3cc7c04f19652eff229ea95f7bf260e0f353e9f0
ThatOneKrish <csrealracing13d@gmail.com>
2022-08-23 17:31:13 +0530

    Update Assignment1.java

--- InterestCalc/Assignment1.java
+++ InterestCalc/Assignment1.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 public class Assignment1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter Your First Name: ");
+        System.out.println("Enter Your First Name: ") ;
         String name1 = input.nextLine();
-        System.out.println("Enter Your Second Name: ");
+        System.out.println("Enter Your Second Name: ") ;
         String name2 = input.nextLine();
         System.out.println("Welcome " + name1 );
-        System.out.println("Enter Your Principle Amount: ");
+        System.out.println("Enter Your Principle Amount: ") ;
         float prin = input.nextFloat();
-        System.out.println("Enter Your Interest Rate: ");
+        System.out.println("Enter Your Interest Rate: ") ;
         float rate = input.nextFloat();
-        System.out.println("Enter the Time(in Years) for Simple Interest: ");
+        System.out.println("Enter the Time(in Years) for Simple Interest: ") ;
         float time = input.nextFloat();
         float inter = prin*rate*time/100 ;
         float simp = prin + inter ;
-        System.out.println("Your Interest for " + time + " years is: " + simp);
+        System.out.println("Your Interest for " + time + " years is: " + simp) ;
 
         if (simp % 2 ==0)
-            System.out.println("YOUR INTEREST NO: "+ simp +" IS EVEN");
+            System.out.println("YOUR INTEREST NO: "+ simp +" IS EVEN") ;
 
         else
-            System.out.println("YOUR INTEREST NO: "+ simp +" IS ODD");
+            System.out.println("YOUR INTEREST NO: "+ simp +" IS ODD") ;
         float curr = simp/80 ;
-        System.out.println("Your Interest in Dollars: " + curr);
+        System.out.println("Your Interest in Dollars: " + curr) ;
 
     }
 }
